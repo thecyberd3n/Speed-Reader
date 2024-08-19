9f3995c2e8c560f0c78de53d5c86d69c24912085
thecyberd3n <thebosscyberden@gmail.com>
2024-08-20 00:21:39 +0100

    sample text changed and made it loop

--- script.js
+++ script.js
@@ -24,12 +24,18 @@ function texttime(){
             idx++;
             if (idx >= words.length) {
                 clearInterval(interval);
+                document.getElementById("text").style.display = 'block';
+                document.getElementById("display").style.display = 'none';
+                document.getElementById("start").style.display = 'block';
+                document.getElementById("title").style.display = 'block';
             }
         }, 70);
+
+ 
     }
 }
 
 
 
 //FOR TESTING
-document.getElementById("text").value = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Pariatur, mollitia accusantium voluptas voluptatem voluptatibus possimus soluta corporis molestiae architecto dicta alias quaerat tenetur et quam expedita aliquam similique quos minus"
+document.getElementById("text").value = "The cat (Felis catus), commonly referred to as the domestic cat or house cat, is a small domesticated carnivorous mammal. It is the only domesticated species of the family Felidae. Recent advances in archaeology and genetics have shown that the domestication of the cat occurred in the Near East around 7500 BC. It is commonly kept as a house pet and farm cat, but also ranges freely as a feral cat avoiding human contact. Valued by humans for companionship and its ability to kill vermin, the cat's retractable claws are adapted to killing small prey like mice and rats. It has a strong, flexible body, quick reflexes, and sharp teeth, and its night vision and sense of smell are well developed. It is a social species, but a solitary hunter and a crepuscular predator. Cat communication includes vocalizations like meowing, purring, trilling, hissing, growling, and grunting as well as cat body language. It can hear sounds too faint or too high in frequency for human ears, such as those made by small mammals. It secretes and perceives pheromones."
